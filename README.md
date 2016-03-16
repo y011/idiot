8cfcbb3afc011f633e65443e72dcd04fde051623
snare <snare@ho.ax>
2016-03-16 23:27:09 +1100

    Docker machine check

--- README.md
+++ README.md
@@ -60,6 +60,7 @@ New checks must be explicitly enabled in config. Edit your `~/.idiot/config` and
     enabled:
       - ApacheCheck
       - FirewallCheck
+      - DockerMachineCheck
       - MyUserCheck
 
 When testing, you might want to edit your config to enable debug loggin, make the checks run more frequently and lower the snooze intervals with a config like this:
---
+++ idiot/checks/docker_machine.py
@@ -0,0 +1,31 @@
+"""
+docker-machine check for Idiot.
+"""
+import logging
+import subprocess
+
+import idiot
+from idiot import CheckPlugin
+
+log = logging.getLogger()
+
+
+class DockerMachineCheck(CheckPlugin):
+    name = "Docker machine"
+
+    def run(self):
+        try:
+            output = subprocess.check_output("docker-machine ls", shell=True).split('\n')[1:]
+            running = [line.split()[0] for line in output if 'Running' in line]
+            if len(running):
+                return (False, "Docker machines are running: {}".format(', '.join(running)))
+            else:
+                return (True, "No Docker machines are running")
+        except Exception as e:
+            log.exception()
+            return (False, "Failed to get `docker-machine ls` output")
+
+
+if __name__ == "__main__":
+    idiot.init()
+    print(DockerMachineCheck().run())
